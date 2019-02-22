9851b9b1d84b302b6a7b43c81ee5a9dd0d41a2a2
Edgar Eloy Munoz <edgareloymunoz@gmail.com>
2019-02-22 12:01:02 -0700

    My working data

--- data.js
+++ data.js
@@ -1,36 +1,584 @@
-const myData = [
-  {"question": "value",
-   id: 1, 
-   "correctAnswer": "answer",
-   "incorrectAnswers": [
-      "wrong1",
-      "wrong2",
-      "wrong3",
-    ],
+const shortHandProblems = [
+  {"prompt": "Pseudocode through the process of toggling the foo value from baz to bar.",
+    "template": "foo = baz? \n changeFoo(bar) \n foo = bar? \n changeFoo(baz) \n changeFoo(caw caw)",
+   "id": 1, 
+   "Answers": [{
+    "1": "baz?"
    },
    {
-    "id": "1",
-    "category": "Prototype methods",
-    "question": "Which array proptotype methods return a representation of an array, but do not modify it?",
-    "answers": ["Accessor methods", "Mutator methods", "Iteration methods"],
-    "correct_answer": "Accessor methods",
-    "resources": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/prototype#Accessor_methods"
+    "2": "(bar)"
    },
    {
-    "question": "What is a package?",
-    "correctAnswer": "One or more modules grouped together that solve a problem.",
-    "incorrectAnswers": [
-      "An app used in React to create components.",
-      "A feature specific to Node.js.",
-      "Code that can't be re-used once it has executed."
-    ],
-    "category": "Concepts",
-    "difficulty": "Easy",
-    "pointsValue": 100
-  },
-  
+    "3": "bar?"
+   },
+   {
+    "4": "(baz)"
+   },
+   {
+    "5": "changeFoo(caw caw)"
+   }],
+   },
+   {"prompt": "Sort the names in the array in alphabetical order based on their first names. \n const names = ['Edgar', 'Kim', 'Jessica'];",
+    "template": "",
+   "id": 2, 
+   "Answers": [{
+    "1": "baz?"
+   },
+   {
+    "2": "(bar)"
+   },
+   {
+    "3": "bar?"
+   },
+   {
+    "4": "(baz)"
+   },
+   {
+    "5": "changeFoo(caw caw)"
+   }],
+   },
+   {"prompt": "Multiply and return each number in the array by 4 \n const numbers = [2, 4, 7, 19];",
+    "template": "here goes the pseudocode template with empty spaces",
+   "id": 3, 
+   "Answers": [{
+    "1": "baz?"
+   },
+   {
+    "2": "(bar)"
+   },
+   {
+    "3": "bar?"
+   },
+   {
+    "4": "(baz)"
+   },
+   {
+    "5": "changeFoo(caw caw)"
+   }],
+   },
+   {"prompt": "Multiply and return each number in the array by 2 \n const nums = [1, '4', 6, '7', '11'];",
+    "template": "here goes the pseudocode template with empty spaces",
+   "id": 4, 
+   "Answers": [{
+    "1": "baz?"
+   },
+   {
+    "2": "(bar)"
+   },
+   {
+    "3": "bar?"
+   },
+   {
+    "4": "(baz)"
+   },
+   {
+    "5": "changeFoo(caw caw)"
+   }],
+   },
+   {"prompt": "Pseudocode through the process of toggling the foo value from baz to bar.",
+    "template": "foo = baz? \n changeFoo(bar) \n foo = bar? \n changeFoo(baz) \n changeFoo(caw caw)",
+   "id": 5, 
+   "Answers": [{
+    "1": "baz?"
+   },
+   {
+    "2": "(bar)"
+   },
+   {
+    "3": "bar?"
+   },
+   {
+    "4": "(baz)"
+   },
+   {
+    "5": "changeFoo(caw caw)"
+   }],
+   },
+   {"prompt": "Pseudocode through the process of toggling the foo value from baz to bar.",
+    "template": "foo = baz? \n changeFoo(bar) \n foo = bar? \n changeFoo(baz) \n changeFoo(caw caw)",
+   "id": 6, 
+   "Answers": [{
+    "1": "baz?"
+   },
+   {
+    "2": "(bar)"
+   },
+   {
+    "3": "bar?"
+   },
+   {
+    "4": "(baz)"
+   },
+   {
+    "5": "changeFoo(caw caw)"
+   }],
+   },
+   {"prompt": "Pseudocode through the process of toggling the foo value from baz to bar.",
+    "template": "foo = baz? \n changeFoo(bar) \n foo = bar? \n changeFoo(baz) \n changeFoo(caw caw)",
+   "id": 7, 
+   "Answers": [{
+    "1": "baz?"
+   },
+   {
+    "2": "(bar)"
+   },
+   {
+    "3": "bar?"
+   },
+   {
+    "4": "(baz)"
+   },
+   {
+    "5": "changeFoo(caw caw)"
+   }],
+   },
+   {"prompt": "Pseudocode through the process of toggling the foo value from baz to bar.",
+    "template": "foo = baz? \n changeFoo(bar) \n foo = bar? \n changeFoo(baz) \n changeFoo(caw caw)",
+   "id": 8, 
+   "Answers": [{
+    "1": "baz?"
+   },
+   {
+    "2": "(bar)"
+   },
+   {
+    "3": "bar?"
+   },
+   {
+    "4": "(baz)"
+   },
+   {
+    "5": "changeFoo(caw caw)"
+   }],
+   },
+   {"prompt": "Pseudocode through the process of toggling the foo value from baz to bar.",
+    "template": "foo = baz? \n changeFoo(bar) \n foo = bar? \n changeFoo(baz) \n changeFoo(caw caw)",
+   "id": 9, 
+   "Answers": [{
+    "1": "baz?"
+   },
+   {
+    "2": "(bar)"
+   },
+   {
+    "3": "bar?"
+   },
+   {
+    "4": "(baz)"
+   },
+   {
+    "5": "changeFoo(caw caw)"
+   }],
+   },
+   {"prompt": "Pseudocode through the process of toggling the foo value from baz to bar.",
+    "template": "foo = baz? \n changeFoo(bar) \n foo = bar? \n changeFoo(baz) \n changeFoo(caw caw)",
+   "id": 10, 
+   "Answers": [{
+    "1": "baz?"
+   },
+   {
+    "2": "(bar)"
+   },
+   {
+    "3": "bar?"
+   },
+   {
+    "4": "(baz)"
+   },
+   {
+    "5": "changeFoo(caw caw)"
+   }],
+   },
+   {"prompt": "Pseudocode through the process of toggling the foo value from baz to bar.",
+    "template": "foo = baz? \n changeFoo(bar) \n foo = bar? \n changeFoo(baz) \n changeFoo(caw caw)",
+   "id": 11, 
+   "Answers": [{
+    "1": "baz?"
+   },
+   {
+    "2": "(bar)"
+   },
+   {
+    "3": "bar?"
+   },
+   {
+    "4": "(baz)"
+   },
+   {
+    "5": "changeFoo(caw caw)"
+   }],
+   },
+   {"prompt": "Pseudocode through the process of toggling the foo value from baz to bar.",
+    "template": "foo = baz? \n changeFoo(bar) \n foo = bar? \n changeFoo(baz) \n changeFoo(caw caw)",
+   "id": 12, 
+   "Answers": [{
+    "1": "baz?"
+   },
+   {
+    "2": "(bar)"
+   },
+   {
+    "3": "bar?"
+   },
+   {
+    "4": "(baz)"
+   },
+   {
+    "5": "changeFoo(caw caw)"
+   }],
+   },
+   {"prompt": "Pseudocode through the process of toggling the foo value from baz to bar.",
+    "template": "foo = baz? \n changeFoo(bar) \n foo = bar? \n changeFoo(baz) \n changeFoo(caw caw)",
+   "id": 13, 
+   "Answers": [{
+    "1": "baz?"
+   },
+   {
+    "2": "(bar)"
+   },
+   {
+    "3": "bar?"
+   },
+   {
+    "4": "(baz)"
+   },
+   {
+    "5": "changeFoo(caw caw)"
+   }],
+   },
+   {"prompt": "Pseudocode through the process of toggling the foo value from baz to bar.",
+    "template": "foo = baz? \n changeFoo(bar) \n foo = bar? \n changeFoo(baz) \n changeFoo(caw caw)",
+   "id": 14, 
+   "Answers": [{
+    "1": "baz?"
+   },
+   {
+    "2": "(bar)"
+   },
+   {
+    "3": "bar?"
+   },
+   {
+    "4": "(baz)"
+   },
+   {
+    "5": "changeFoo(caw caw)"
+   }],
+   },
+   {"prompt": "Pseudocode through the process of toggling the foo value from baz to bar.",
+    "template": "foo = baz? \n changeFoo(bar) \n foo = bar? \n changeFoo(baz) \n changeFoo(caw caw)",
+   "id": 15, 
+   "Answers": [{
+    "1": "baz?"
+   },
+   {
+    "2": "(bar)"
+   },
+   {
+    "3": "bar?"
+   },
+   {
+    "4": "(baz)"
+   },
+   {
+    "5": "changeFoo(caw caw)"
+   }],
+   },
+   {"prompt": "Pseudocode through the process of toggling the foo value from baz to bar.",
+    "template": "foo = baz? \n changeFoo(bar) \n foo = bar? \n changeFoo(baz) \n changeFoo(caw caw)",
+   "id": 16, 
+   "Answers": [{
+    "1": "baz?"
+   },
+   {
+    "2": "(bar)"
+   },
+   {
+    "3": "bar?"
+   },
+   {
+    "4": "(baz)"
+   },
+   {
+    "5": "changeFoo(caw caw)"
+   }],
+   },
+   {"prompt": "Pseudocode through the process of toggling the foo value from baz to bar.",
+    "template": "foo = baz? \n changeFoo(bar) \n foo = bar? \n changeFoo(baz) \n changeFoo(caw caw)",
+   "id": 17, 
+   "Answers": [{
+    "1": "baz?"
+   },
+   {
+    "2": "(bar)"
+   },
+   {
+    "3": "bar?"
+   },
+   {
+    "4": "(baz)"
+   },
+   {
+    "5": "changeFoo(caw caw)"
+   }],
+   },
+   {"prompt": "Pseudocode through the process of toggling the foo value from baz to bar.",
+    "template": "foo = baz? \n changeFoo(bar) \n foo = bar? \n changeFoo(baz) \n changeFoo(caw caw)",
+   "id": 18, 
+   "Answers": [{
+    "1": "baz?"
+   },
+   {
+    "2": "(bar)"
+   },
+   {
+    "3": "bar?"
+   },
+   {
+    "4": "(baz)"
+   },
+   {
+    "5": "changeFoo(caw caw)"
+   }],
+   },
+   {"prompt": "Pseudocode through the process of toggling the foo value from baz to bar.",
+    "template": "foo = baz? \n changeFoo(bar) \n foo = bar? \n changeFoo(baz) \n changeFoo(caw caw)",
+   "id": 19, 
+   "Answers": [{
+    "1": "baz?"
+   },
+   {
+    "2": "(bar)"
+   },
+   {
+    "3": "bar?"
+   },
+   {
+    "4": "(baz)"
+   },
+   {
+    "5": "changeFoo(caw caw)"
+   }],
+   },
+   {"prompt": "Pseudocode through the process of toggling the foo value from baz to bar.",
+    "template": "foo = baz? \n changeFoo(bar) \n foo = bar? \n changeFoo(baz) \n changeFoo(caw caw)",
+   "id": 20, 
+   "Answers": [{
+    "1": "baz?"
+   },
+   {
+    "2": "(bar)"
+   },
+   {
+    "3": "bar?"
+   },
+   {
+    "4": "(baz)"
+   },
+   {
+    "5": "changeFoo(caw caw)"
+   }],
+   },
+   {"prompt": "Pseudocode through the process of toggling the foo value from baz to bar.",
+    "template": "foo = baz? \n changeFoo(bar) \n foo = bar? \n changeFoo(baz) \n changeFoo(caw caw)",
+   "id": 21, 
+   "Answers": [{
+    "1": "baz?"
+   },
+   {
+    "2": "(bar)"
+   },
+   {
+    "3": "bar?"
+   },
+   {
+    "4": "(baz)"
+   },
+   {
+    "5": "changeFoo(caw caw)"
+   }],
+   },
+   {"prompt": "Pseudocode through the process of toggling the foo value from baz to bar.",
+    "template": "foo = baz? \n changeFoo(bar) \n foo = bar? \n changeFoo(baz) \n changeFoo(caw caw)",
+   "id": 22, 
+   "Answers": [{
+    "1": "baz?"
+   },
+   {
+    "2": "(bar)"
+   },
+   {
+    "3": "bar?"
+   },
+   {
+    "4": "(baz)"
+   },
+   {
+    "5": "changeFoo(caw caw)"
+   }],
+   },
+   {"prompt": "Pseudocode through the process of toggling the foo value from baz to bar.",
+    "template": "foo = baz? \n changeFoo(bar) \n foo = bar? \n changeFoo(baz) \n changeFoo(caw caw)",
+   "id": 23, 
+   "Answers": [{
+    "1": "baz?"
+   },
+   {
+    "2": "(bar)"
+   },
+   {
+    "3": "bar?"
+   },
+   {
+    "4": "(baz)"
+   },
+   {
+    "5": "changeFoo(caw caw)"
+   }],
+   },
+   {"prompt": "Pseudocode through the process of toggling the foo value from baz to bar.",
+    "template": "foo = baz? \n changeFoo(bar) \n foo = bar? \n changeFoo(baz) \n changeFoo(caw caw)",
+   "id": 24, 
+   "Answers": [{
+    "1": "baz?"
+   },
+   {
+    "2": "(bar)"
+   },
+   {
+    "3": "bar?"
+   },
+   {
+    "4": "(baz)"
+   },
+   {
+    "5": "changeFoo(caw caw)"
+   }],
+   },
+   {"prompt": "Pseudocode through the process of toggling the foo value from baz to bar.",
+    "template": "foo = baz? \n changeFoo(bar) \n foo = bar? \n changeFoo(baz) \n changeFoo(caw caw)",
+   "id": 25, 
+   "Answers": [{
+    "1": "baz?"
+   },
+   {
+    "2": "(bar)"
+   },
+   {
+    "3": "bar?"
+   },
+   {
+    "4": "(baz)"
+   },
+   {
+    "5": "changeFoo(caw caw)"
+   }],
+   },
+   {"prompt": "Pseudocode through the process of toggling the foo value from baz to bar.",
+    "template": "foo = baz? \n changeFoo(bar) \n foo = bar? \n changeFoo(baz) \n changeFoo(caw caw)",
+   "id": 26, 
+   "Answers": [{
+    "1": "baz?"
+   },
+   {
+    "2": "(bar)"
+   },
+   {
+    "3": "bar?"
+   },
+   {
+    "4": "(baz)"
+   },
+   {
+    "5": "changeFoo(caw caw)"
+   }],
+   },
+   {"prompt": "Pseudocode through the process of toggling the foo value from baz to bar.",
+    "template": "foo = baz? \n changeFoo(bar) \n foo = bar? \n changeFoo(baz) \n changeFoo(caw caw)",
+   "id": 27, 
+   "Answers": [{
+    "1": "baz?"
+   },
+   {
+    "2": "(bar)"
+   },
+   {
+    "3": "bar?"
+   },
+   {
+    "4": "(baz)"
+   },
+   {
+    "5": "changeFoo(caw caw)"
+   }],
+   },
+   {"prompt": "Pseudocode through the process of toggling the foo value from baz to bar.",
+    "template": "foo = baz? \n changeFoo(bar) \n foo = bar? \n changeFoo(baz) \n changeFoo(caw caw)",
+   "id": 28, 
+   "Answers": [{
+    "1": "baz?"
+   },
+   {
+    "2": "(bar)"
+   },
+   {
+    "3": "bar?"
+   },
+   {
+    "4": "(baz)"
+   },
+   {
+    "5": "changeFoo(caw caw)"
+   }],
+   },
+   {"prompt": "Pseudocode through the process of toggling the foo value from baz to bar.",
+    "template": "foo = baz? \n changeFoo(bar) \n foo = bar? \n changeFoo(baz) \n changeFoo(caw caw)",
+   "id": 29, 
+   "Answers": [{
+    "1": "baz?"
+   },
+   {
+    "2": "(bar)"
+   },
+   {
+    "3": "bar?"
+   },
+   {
+    "4": "(baz)"
+   },
+   {
+    "5": "changeFoo(caw caw)"
+   }],
+   },
+   {"prompt": "Pseudocode through the process of toggling the foo value from baz to bar.",
+    "template": "foo = baz? \n changeFoo(bar) \n foo = bar? \n changeFoo(baz) \n changeFoo(caw caw)",
+   "id": 30, 
+   "Answers": [{
+    "1": "baz?"
+   },
+   {
+    "2": "(bar)"
+   },
+   {
+    "3": "bar?"
+   },
+   {
+    "4": "(baz)"
+   },
+   {
+    "5": "changeFoo(caw caw)"
+   }],
+   }
+]
+
+const pseudoCodingProblems = [
+  {"prompt": "Pseudocode through the process of making a sandwhich",
+   "id": 31, 
+   "Answer": 
+   }
 ]
 
 module.exports = {
-  myData
+  shortHandProblems,
+  pseudoCodingProblems
 };
